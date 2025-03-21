3884b87a43fcfe19d7bd5313b2b097d017d8163f
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-03-21 14:56:17 +0800

    Time: 453 ms (12.89%), Space: 19.6 MB (79.98%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.py
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.py
@@ -1,28 +1,53 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-        ingred = set()
-        res = []
-        flag_add = True
+        # ingred = set()
+        # res = []
+        # flag_add = True
 
-        for i in supplies:
-            ingred.add(i)
+        # for i in supplies:
+        #     ingred.add(i)
         
-        while flag_add == True:
-            init_len = len(res)
-            for r, i in zip(recipes, ingredients):
-                flag_ingred = True
-                for j in i:
-                    if j not in ingred:
-                        flag_ingred = False
+        # while flag_add == True:
+        #     init_len = len(res)
+        #     for r, i in zip(recipes, ingredients):
+        #         flag_ingred = True
+        #         for j in i:
+        #             if j not in ingred:
+        #                 flag_ingred = False
+        #                 break
+        #         if flag_ingred == True and r not in res:
+        #             ingred.add(r)
+        #             res.append(r)
+        #     if len(res) == init_len:
+        #         flag_add = False
+        
+        # return res
+
+        supplies = set(supplies)
+        dic = {recipes[i] : ingredients[i] for i in range(len(ingredients))}
+        to_add, res = [], []
+
+        start = True
+        while to_add or start:
+            start = False
+
+            # Update the supply and take away recipes we alr made
+            for i in to_add:
+                del dic[i]
+                supplies.add(i)
+                res.append(i)
+
+            # See which recipes can be made with current supplies
+            to_add = []
+            for key, val in dic.items():
+                can_make = True
+                for i in val:
+                    if i not in supplies:
+                        can_make = False
                         break
-                if flag_ingred == True and r not in res:
-                    ingred.add(r)
-                    res.append(r)
-            if len(res) == init_len:
-                flag_add = False
-        
+                if can_make:
+                    to_add.append(key)
         return res
-
 
             
             
