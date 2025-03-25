acfec38c9f79df79313f09c4a736fc10bb78b64f
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-03-25 11:09:48 +0800

    Time: 8 ms (50.02%), Space: 20.9 MB (89.33%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,29 +1,41 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # intervals = sorted(intervals)
+        # res = []
+        # cur = []
+
+        # for i in intervals:
+        #     if res == []:
+        #         res.append(i)
+        #         cur = res[-1]
+        #     if i[0] >= cur[0] and i[1] <= cur[1]:
+        #         continue
+        #     elif i[0] > cur[1]:
+        #         res.append(i)
+        #         cur = res[-1]
+        #     else:
+        #         if i[1] > cur[1]:
+        #             res[-1][1] = i[1]
+        #             cur[1] = i[1]
+        #         if i[0] < cur[0]:
+        #             res[-1][0] = i[0]
+        #             cur[0] = i[0]
+        # return res    
+        if not intervals:
+            return []
+
         intervals = sorted(intervals)
-        res = []
-        cur = []
+        res = [intervals[0]]
 
-        for i in intervals:
-            if res == []:
-                res.append(i)
-                cur = res[-1]
-            if i[0] >= cur[0] and i[1] <= cur[1]:
-                continue
-            elif i[0] > cur[1]:
-                res.append(i)
-                cur = res[-1]
+        for i in intervals[1:]:
+            cur = res[-1]
+            start, end = cur[0], cur[1]
+
+            if i[0] <= cur[1]:
+                res[-1][1] = max(i[1], res[-1][1])
             else:
-                if i[1] > cur[1]:
-                    res[-1][1] = i[1]
-                    cur[1] = i[1]
-                if i[0] < cur[0]:
-                    res[-1][0] = i[0]
-                    cur[0] = i[0]
-        return res    
+                res.append(i)
 
+        return res
 
-
-
-                
-            
+       
