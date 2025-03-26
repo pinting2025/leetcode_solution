65e1f1636036b9583c74d1b584efd5179cb4c545
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-03-26 12:51:32 +0800

    Time: 166 ms (49.12%), Space: 38.7 MB (97.37%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.py
@@ -6,31 +6,19 @@ class Solution:
                 flatten.append(c)
         flatten.sort()
         
-        def is_valid(x):
-            remainder = flatten[0] % x
-            for i in flatten[1:]:
-                if i % x != remainder:
-                    return False
-            return True
+        remainder = flatten[0] % x
+        for i in flatten[1:]:
+            if i % x != remainder:
+                return -1
         
         def count_step(target):
             count = 0
             for i in flatten:
                 count += abs(i - target) // x
             return count
-        
-        if not is_valid(x):
-            return -1
-        else:
-            res = float('inf')
-            mid = len(flatten) // 2
-            while mid < len(flatten):
-                if count_step(flatten[mid]) < res:
-                    res = count_step(flatten[mid])
-                    mid += 1
-                else:
-                    break
-            return res
+
+        median = len(flatten) // 2
+        return count_step(flatten[median])
 
 
 
