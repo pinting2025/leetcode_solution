620c1f262a0e888f19824987e6a4762eb8fbc615
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-03-30 10:30:20 +0800

    Time: 11 ms (10.4%), Space: 17.8 MB (57.51%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -1,25 +1,28 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         appear = Counter(s)
+        cur_element = Counter()
 
         left = 0
         right = 0
+        remain = 0
         res = []
 
-        cur_element = Counter()
-
         while right < len(s):
-            if s[right] in cur_element:
-                cur_element[s[right]] -= 1
-            else:
+            if cur_element[s[right]] == 0:
                 cur_element[s[right]] += appear[s[right]] - 1
+                remain += cur_element[s[right]]
+            else:
+                cur_element[s[right]] -= 1
+                remain -= 1
             
             right += 1
 
-            if sum(cur_element.values()) == 0:
+            if remain == 0:
                 res.append(right-left)
                 left = right
-        
+                cur_element.clear()
+
         return res
                 
 
