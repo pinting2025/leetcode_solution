1e9433598002267ca425de4cff1d071c140e3639
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-04-09 18:06:35 +0800

    Time: 4 ms (8.37%), Space: 17.8 MB (65.7%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,17 +1,46 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        # bottom-up
         matrix = [[0 for i in range(n)] for j in range(m)]
 
-        for i in range(m):
-            for j in range(n):
-                if i == 0 or j == 0:
-                    matrix[i][j] = 1
+        for r in range(m):
+            for c in range(n):
+                if r == 0 or c == 0:
+                    matrix[r][c] = 1
                 else:
-                    matrix[i][j] = matrix[i-1][j] + matrix[i][j-1]
-        
+                    matrix[r][c] = matrix[r-1][c] + matrix[r][c-1]
+    
         return matrix[-1][-1]
 
 
 
-        
-        
+
+
+
+
+
+
+
+
+
+
+
+
+        # top-down
+        matrix = [[None for i in range(n)] for j in range(m)]
+        def dp(r, c):
+            if r < 0 or c < 0 or r >= m or c >= n:
+                return 0
+
+            if matrix[r][c] != None:
+                return matrix[r][c]
+            
+            if r == 0 or c == 0:
+                matrix[r][c] = 1
+                return 1
+            
+            matrix[r][c] = dp(r-1, c) + dp(r, c-1)      
+
+            return matrix[r][c]
+
+        return dp(n, m)
