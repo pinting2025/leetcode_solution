8bdf1fde2c21fab6163cd619c6bfb4d3126a4a4c
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-04-09 15:00:37 +0800

    Time: 7 ms (21.01%), Space: 20.3 MB (6.92%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,31 +1,28 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # # top-down
-        # memo = {}
-        # def dp(i):
-        #     if i in memo:
-        #         return memo[i]
+        # top-down
+        memo = {}
+        def dp(i):
+            if i in memo:
+                return memo[i]
             
-        #     if i >= len(cost):
-        #         return 0
-            
-        #     take1 = dp(i+1, val+cost[i])
-        #     take2 = dp(i+2, val+cost[i])
+            if i >= len(cost):
+                return 0
 
-        #     memo[i] = min(take1, take2)
+            memo[i] = cost[i] + min(dp(i+1), dp(i+2))
 
-        #     return memo[i]
+            return memo[i]
         
-        # return min(dp(0, cost[0]), dp(1, cost[1]))
+        return min(dp(0), dp(1))
         
-        # bottom-up
-        if len(cost) == 2:
-            return min(cost)
+        # # bottom-up
+        # if len(cost) == 2:
+        #     return min(cost)
 
-        dp = [0] * (len(cost)+1)
-        dp[-2] = cost[-1]
-        for i in range(len(cost)-2, -1, -1):
-            dp[i] = cost[i] + min(dp[i+1], dp[i+2])
-        return min(dp[0], dp[1])
+        # dp = [0] * (len(cost)+1)
+        # dp[-2] = cost[-1]
+        # for i in range(len(cost)-2, -1, -1):
+        #     dp[i] = cost[i] + min(dp[i+1], dp[i+2])
+        # return min(dp[0], dp[1])
 
 
