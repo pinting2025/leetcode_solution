90b4d1e5d332849026b72adb6e8ace5d0174d1c0
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-04-30 14:34:53 +0800

    Time: 153 ms (43.92%), Space: 32.5 MB (16.05%) - LeetHub

--- 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.py
+++ 2249-count-the-hidden-sequences/2249-count-the-hidden-sequences.py
@@ -1,12 +1,39 @@
 class Solution:
     def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
-        up, down = 0, 0
-        start = 0
-        for i in differences:
-            start += i
-            up = max(up, start)
-            down = min(down, start)
+        cur = max_diff = min_diff = 0
 
-        diff = down - lower
-        res = upper - (up - diff) + 1
-        return res if res >= 0 else 0 
+        for d in differences:
+            cur += d
+            max_diff = max(max_diff, cur)
+            min_diff = min(min_diff, cur)
+        
+        res = (upper - max_diff) - (lower - min_diff) + 1
+
+        return res if res > 0 else 0
+
+        # max_diff = max(differences)
+        # min_diff = min(differences)
+        # if abs(max_diff) > upper - lower or abs(min_diff) > upper - lower:
+        #     return 0
+        
+        # res = 0
+        # for i in range(lower, upper+1):
+        #     flag = True
+        #     cur = i
+        #     for j in differences:
+        #         if cur + j < lower or cur + j > upper:
+        #             flag = False
+
+        #         if flag == False:
+        #             break
+        #         cur += j
+
+        #     if flag == True:
+        #         res += 1
+        
+        # return res
+            
+            
+                
+
+        
