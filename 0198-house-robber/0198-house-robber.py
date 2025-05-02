484742daeda835605f6a30187b4ea497f41dbd35
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-05-02 16:53:43 +0800

    Time: 0 ms (100%), Space: 17.9 MB (34.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,6 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
         # bottom-up
+        dp = [0] * 3
+        dp[0] = nums[-1]
+        dp[1] = max(nums[-1], nums[-2])
+
+        for i in range(len(nums)-3, -1, -1):
+            dp[2] = max(dp[0]+nums[i], dp[1])
+            dp[0] = dp[1]
+            dp[1] = dp[2]
+        
+        return dp[2] if len(nums) > 2 else dp[len(nums)-1]
 
         # top-down
         memo = {}
