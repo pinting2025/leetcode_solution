f984802cefad452e1a953ac7b8fe46060fb36d07
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-05-02 18:44:25 +0800

    Time: 19 ms (35.99%), Space: 20.1 MB (61.09%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,27 +1,27 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        # top-down
-        memo = {}
+        # # top-down
+        # memo = {}
 
-        def dp(i, j):
-            if (i, j) in memo:
-                return memo[(i, j)]
+        # def dp(i, j):
+        #     if (i, j) in memo:
+        #         return memo[(i, j)]
             
-            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
-                return float('inf')
+        #     if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
+        #         return float('inf')
             
-            if i == 0 and j == 0:
-                memo[(i, j)] = grid[0][0]
-            elif i == 0:
-                memo[(i, j)] = dp(i, j-1) + grid[i][j]
-            elif j == 0:
-                memo[(i, j)] = dp(i-1, j) + grid[i][j]
-            else:
-                memo[(i, j)] = min(dp(i-1, j), dp(i, j-1)) + grid[i][j]
+        #     if i == 0 and j == 0:
+        #         memo[(i, j)] = grid[0][0]
+        #     elif i == 0:
+        #         memo[(i, j)] = dp(i, j-1) + grid[i][j]
+        #     elif j == 0:
+        #         memo[(i, j)] = dp(i-1, j) + grid[i][j]
+        #     else:
+        #         memo[(i, j)] = min(dp(i-1, j), dp(i, j-1)) + grid[i][j]
 
-            return memo[(i, j)]
+        #     return memo[(i, j)]
         
-        return dp(len(grid)-1, len(grid[0])-1)
+        # return dp(len(grid)-1, len(grid[0])-1)
 
         # bottom-up
         matrix = [[-1 for _ in range(len(grid[0]))] for _ in range(len(grid))]
