15c77018d89e964ad5234aabe926da16d1d456eb
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-05-04 10:55:56 +0800

    Time: 0 ms (100%), Space: 18.9 MB (78.2%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,17 +1,28 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        memo = {}
+        # bottom-up
+        dp = triangle[-1]
+        print(dp)
 
-        def dp(idx, row):
-            if (idx, row) in memo:
-                return memo[(idx, row)]
-            
-            if row >= len(triangle) or idx >= len(triangle[row]):
-                return 0
-            
-            memo[(idx, row)] = min(dp(idx+1, row+1), dp(idx, row+1)) + triangle[row][idx]
+        for r in range(len(triangle)-2, -1, -1):
+            for i in range(len(triangle[r])):
+                dp[i] = triangle[r][i] + min(dp[i], dp[i+1])
 
-            return memo[(idx, row)]
+        return dp[0]
+
+        # # top-down
+        # memo = {}
+
+        # def dp(idx, row):
+        #     if (idx, row) in memo:
+        #         return memo[(idx, row)]
+            
+        #     if row >= len(triangle) or idx >= len(triangle[row]):
+        #         return 0
+            
+        #     memo[(idx, row)] = min(dp(idx+1, row+1), dp(idx, row+1)) + triangle[row][idx]
+
+        #     return memo[(idx, row)]
         
-        return dp(0,0)
+        # return dp(0,0)
 
