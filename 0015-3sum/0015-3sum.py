13e205d72d592dfecaf1f3a605a481fcfa28e5ee
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-05-05 13:49:10 +0800

    Time: 599 ms (45.46%), Space: 20.3 MB (92.39%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,52 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # dic = Counter(nums)
-        # nums = sorted(nums)
-        # ans = []
-
-        # for i in range(len(nums)):
-        #     for j in range(i+1, len(nums)):
-        #         dic[nums[i]] -= 1
-        #         dic[nums[j]] -= 1
-                
-        #         if (0 - (nums[i] + nums[j]) in nums[j+1:]) and dic[0 - (nums[i] + nums[j])] != 0:
-        #             # temp = sorted([nums[i], nums[j], (0 - (nums[i] + nums[j]))])
-        #             # if temp not in ans:
-        #             ans.append([nums[i], nums[j], (0 - (nums[i] + nums[j]))])
-                
-        #         dic[nums[i]] += 1
-        #         dic[nums[j]] += 1
-        
-        # return ans
-        
         nums = sorted(nums)
-
-        def two_sum(numbers, target):
-            seen = set()
-            pairs = []
-
-            for j in range(len(numbers)):
-                complement = target - numbers[j]
-                if complement in seen:
-                    # only add this pair if we haven't added the same pair before
-                    if not pairs or pairs[-1] != [complement, numbers[j]]:
-                        pairs.append([complement, numbers[j]])
-                seen.add(numbers[j])
-            return pairs
-        
-        ans_p = []
+        res = set()
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
+            if i != 0 and nums[i] == nums[i-1]:
                 continue
 
-            p = two_sum(nums[i+1:], 0 - nums[i])
+            left = i + 1
+            right = len(nums) - 1
 
-            if p != []:
-                for n1, n2 in p:
-                    ans_p.append([nums[i], n1, n2])
+            while left < right:
+                cur = nums[left] + nums[right] + nums[i]
+                if cur == 0:
+                    temp = tuple(sorted([nums[left], nums[right], nums[i]]))
+                    if temp not in res:
+                        res.add(temp)
+                    left += 1
+                    right -= 1
+                
+                else:
+                    if cur < 0:
+                        left += 1
+                    else:
+                        right -= 1
         
-        return ans_p
-
-            
-
-                    
+        return list(res)
