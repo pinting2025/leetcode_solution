52e63f88b1ef21dc170331c6b55a35991b4b8768
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-05-20 17:38:41 +0800

    Time: 167 ms (19.85%), Space: 63.9 MB (36.72%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -1,39 +1,18 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        # bottom-up
-        dp = [0] * (len(questions) + 1)
-
-        for i in range(len(questions)-1, -1, -1):
-            p, nextq = questions[i]
-
-            skip = dp[i+1]
-
-            if i + nextq + 1 < len(questions):
-                take = dp[i + nextq + 1] + p
-            else:
-                take = p
-            
-            dp[i] = max(take, skip)
-        
-        return dp[0]
-
-        # top-down
         memo = {}
 
         def dp(idx):
-            if idx in memo:
-                return memo[idx]
-            
             if idx >= len(questions):
                 return 0
             
-            # skip
+            if idx in memo:
+                return memo[idx]
+            
             skip = dp(idx+1)
+            take = questions[idx][0] + dp(idx+questions[idx][1]+1)
 
-            # solve 
-            solve = questions[idx][0] + dp(idx+questions[idx][1]+1) 
-
-            memo[idx] = max(skip, solve)
+            memo[idx] = max(skip, take)
 
             return memo[idx]
         
