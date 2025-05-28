0e961ff33c73c12db0448af9c5c8afb5d808608f
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-05-28 14:04:27 +0800

    Time: 20 ms (29.53%), Space: 23.9 MB (7.19%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,8 +1,27 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        res = [0] * (n + 1)
+        # top-down
+        memo = {}
 
+        def dp(idx):
+            if idx in memo:
+                return memo[idx]
+            
+            if idx == 1:
+                return 1
+
+            memo[i] = dp(i >> 1) + (i & 1)
+            return memo[idx]
+        
+        res = [0] * (n+1)
         for i in range(1, n+1):
-            res[i] = res[i//2] + (i & 1)
+            res[i] = dp(i)
 
         return res
+
+        # res = [0] * (n + 1)
+
+        # for i in range(1, n+1):
+        #     res[i] = res[i//2] + (i & 1)
+
+        # return res
