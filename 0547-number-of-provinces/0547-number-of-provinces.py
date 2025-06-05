b8c8c3cbb0785dd427f7a838c010193fb24e1e2c
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-06-05 11:47:22 +0800

    Time: 3 ms (89.41%), Space: 19.3 MB (27.24%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,30 +1,52 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        parent = [i for i in range(len(isConnected))]
+        # dfs
+        visited = set()
+        res = 0
 
-        def find(i):
-            if parent[i] != i:
-                parent[i] = find(parent[i])
-            return parent[i]
+        def dfs(i):
+            edge = isConnected[i]
 
-        def connect(i, j):
-            root_i = find(i)
-            root_j = find(j)
-
-            if root_i != root_j:
-                if root_i < root_j:
-                    parent[root_j] = root_i
-                else:
-                    parent[root_i] = root_j
-            
-        for i in range(len(isConnected)):
-            for j in range(i+1, len(isConnected)):
+            for j in range(len(edge)):
                 if isConnected[i][j] == 1:
-                    connect(i, j)
-        
-        res = [find(i) for i in parent]
+                    if j not in visited:
+                        visited.add(j)
+                        dfs(j)
 
-        return len(set(res))
+        for i in range(len(isConnected)):
+            if i not in visited:
+                res += 1
+                visited.add(i)
+                dfs(i)
+
+        return res
+
+        # union find
+        # parent = [i for i in range(len(isConnected))]
+
+        # def find(i):
+        #     if parent[i] != i:
+        #         parent[i] = find(parent[i])
+        #     return parent[i]
+
+        # def connect(i, j):
+        #     root_i = find(i)
+        #     root_j = find(j)
+
+        #     if root_i != root_j:
+        #         if root_i < root_j:
+        #             parent[root_j] = root_i
+        #         else:
+        #             parent[root_i] = root_j
+            
+        # for i in range(len(isConnected)):
+        #     for j in range(i+1, len(isConnected)):
+        #         if isConnected[i][j] == 1:
+        #             connect(i, j)
+        
+        # res = [find(i) for i in parent]
+
+        # return len(set(res))
 
 
 
