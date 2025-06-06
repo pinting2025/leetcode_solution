ee526668588aadf39cc940fe104c05fe1979b0f0
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-06-06 16:04:25 +0800

    Time: 0 ms (100%), Space: 17.8 MB (64.31%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,20 +6,38 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        def is_same(node1, node2):
-            if not node1 and not node2:
-                return True
-            
-            if not node1 or not node2:
-                return False
-            
-            if node1.val != node2.val:
-                return False
-            
-            return is_same(node1.right, node2.right) and is_same(node1.left, node2.left)
-        
-        return is_same(p, q)
+        # return p == q, not correct since we're not checking if two treenode are pointing to the same memory
 
-        
+        queue1 = deque([p])
+        queue2 = deque([q])
 
+        while queue1 and queue2:
+            length1 = len(queue1)
+            length2 = len(queue2)
+            if length1 != length2:
+                return False
+
+            for _ in range(length1):
+                cur1 = queue1.pop()
+                cur2 = queue2.pop()
+
+                if not cur1 and not cur2:
+                    continue
+                
+                if not cur1 or not cur2:
+                    return False
+
+                if cur1.val != cur2.val:
+                    return False
+                
+                queue1.append(cur1.left)
+                queue2.append(cur2.left)
+                queue1.append(cur1.right)
+                queue2.append(cur2.right)
         
+        if queue1 or queue2:
+            return False
+            
+        return True
+
+
