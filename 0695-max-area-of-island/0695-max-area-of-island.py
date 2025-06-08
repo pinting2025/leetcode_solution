579cabe52ff075def63a263f8c68f18633a732ac
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-06-08 13:06:06 +0800

    Time: 27 ms (43.72%), Space: 19.7 MB (11.26%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -4,28 +4,17 @@ class Solution:
         visited = set()
 
         def dfs(i, j):
-            nonlocal temp
-
             if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0 or (i, j) in visited:
-                return 
+                return 0
             
             visited.add((i, j))
 
-            if grid[i][j] == 1:
-                temp += 1
-                dfs(i-1, j)
-                dfs(i, j-1)
-                dfs(i+1, j)
-                dfs(i, j+1)
-
-            return 
+            return 1 + dfs(i-1, j) + dfs(i, j-1) + dfs(i+1, j) + dfs(i, j+1)
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1 and (i, j) not in visited:
-                    temp = 0
-                    dfs(i, j)
-                    res = max(res, temp)
+                    res = max(res, dfs(i, j))
         
         return res
 
