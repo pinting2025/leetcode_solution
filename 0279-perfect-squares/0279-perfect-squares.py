7964d88c5b42de41fd655cd1e22972df169b4e7b
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-08-12 07:54:35 +0200

    Time: 4463 ms (9.63%), Space: 24.2 MB (16.31%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,31 +1,32 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        memo = {} # remain
-
         dic = []
-        for i in range(1, 101):
-            dic.append(i**2)
-
+        for i in range(1, int(sqrt(n)+1)):
+            dic.append(i*i)
+        
+        memo = {}
         def dp(remain):
             if remain == 0:
                 return 0
-
+            
             if remain in memo:
                 return memo[remain]
-            
+
             temp = float('inf')
-            for i in dic:
-                if remain - i >= 0:
-                    temp = min(temp, dp(remain-i) + 1)
+            for d in dic:
+                if d <= remain:
+                    temp = min(temp, dp(remain-d) + 1)
                 else:
                     break
-                
-            memo[remain] = temp
 
+            memo[remain] = temp
             return memo[remain]
 
         return dp(n)
-                
 
-        
-        
+            
+
+
+            
+
+            
