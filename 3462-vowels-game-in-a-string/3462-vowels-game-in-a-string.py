ea7a543d86514e089761944d9828a61f5598c6ce
qup3wu/6 <pt.liao.2023@smu.edu.sg>
2025-09-12 05:55:25 +0200

    Time: 8 ms (83.64%), Space: 18.3 MB (7.58%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.py
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.py
@@ -1,20 +1,24 @@
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
         # they will take the longest available string each time
-        turn = 0 # keep track of who's turn it is 
-        string = list(s)
         volwels = {"a", "e", "i", "o", "u"}
-        volwels_list = []
+        s = set(s)
+        return False if volwels.isdisjoint(s) else True  
 
-        for i in range(len(string)):
-            if string[i] in volwels:
-                volwels_list.append(i)
 
-        if len(volwels_list) == 0:
-            return False
-        else:
-            return True
-            
+
+
+        # volwels_list = []
+
+        # for i in range(len(string)):
+        #     if string[i] in volwels:
+        #         volwels_list.append(i)
+
+        # if len(volwels_list) == 0:
+        #     return False
+        # else:
+        #     return True
+
         # while volwels_list or (turn == 1 and len(volwels_list) % 2 == 0):
         #     cur = 0
         #     if turn == 0: # Alice
